94d85c357597c7060c802895c58ea272a6115254
Shaonkabir <shaonkabir98@gmail.com>
2020-01-22 22:11:32 +0600

    Feat: (add Blog page. It'll be updated later)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,34 @@
 import React from "react"
+import Header from "../components/Header"
 
-const blog = props => {
+const Blog = () => {
 	return (
-		<div>
-			<h3>blog page content</h3>
-			<p>
-				ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptas dolore
-				id quam eveniet amet rerum doloribus, nisi quo facere suscipit quasi
-				atque hic! Reprehenderit rem placeat iure est, non.
-			</p>
+		<div className="blog-hero-area">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-10 offset-md-1">
+						<h1>শাওনের ডায়রি</h1>
+						<p>
+							আমি শাওন কবির। ভালোবাসি গান শুনতে, গল্প লিখতে, ঘুরতে। অনেকদিন
+							থেকেই একটা ইচ্ছা ছিল নিজের অদ্ভত ভাবনাগুলো কোথাও না কোথাও লিখবো ।
+							সবাই পড়বে, দেখবে। অবশেষে নিজের একটা ওয়েবসাইট খুলে তাতে ব্লগ লেখা
+							শুরু করলাম । এতদিন যে লেখাগুলো গুগল কীপ বা ডায়রি তে লিখতাম সেই
+							লেখাগুলোই ব্লগে লিখবো। এই ব্লগের সবটা জুড়েই এক স্বপ্ন বাজ ভবঘুরে
+							ছেলের একরাশ খামখেয়ালী ।
+						</p>
+						<p>
+							আমার সম্পর্কে এর চেয়ে বেশি লেখার সৌভাগ্য আমার এখনো হইনি। হাসি হাসি
+							মুখে দিব্বি বেঁচে আছি এটাই বিধিতার সবচেয়ে বড় কৃপা।
+						</p>
+						<p>
+							যদি লোকসম্মুখে প্রশ্ন জিগ্গেস করতে বা উপদেশ, বকাঝকা, গালাগালি,
+							হুমকি দিতে সংকোচ লাগে তাইলে ইমেইল করে দেন shaonkabir98@gmail.com
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
 
-export default blog
+export default Blog
