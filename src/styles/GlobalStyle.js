7651afec40ebfa1d9e70c88d3ab5a324b2a83552
Shaonkabir <shaonkabir98@gmail.com>
2020-01-22 16:07:28 +0600

    style: (update Global Style)

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -10,7 +10,31 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: 'Merriweather',serif;
+    font-size: 16px;
+    line-height: 1.7;
   }
+    h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		margin: 0 0 15px;
+		font-weight: 700;
+		font-weight: 700;
+		font-family: 'Lora', serif;
+	}
+
+  a:hover {text-decoration:none}
+  ol,ul {
+  	margin: 0;
+  	padding: 0;
+  	list-style:none;
+  }
+	.section-padding {
+		padding: 80px 0;
+	}
+
 `
 
 export default GlobalStyle
