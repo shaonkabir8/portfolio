2df78c09072365d2bdbe52de2ac3aee052949881
Shaonkabir <shaonkabir98@gmail.com>
2020-01-23 12:42:28 +0600

    Feat: (add gatsby-image plugin for lazy loading)

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,8 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-styled-components`,
-
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
