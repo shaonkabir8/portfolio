222f782cf8735aebfd3e159c2f7988e34c8c452d
Shaonkabir <shaonkabir98@gmail.com>
2020-01-23 22:48:34 +0600

    Test: (test gatsby-images with query for portfolio item)

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,61 +1,101 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 import PortfolioCardStyle from "../styles/PortfolioCardStyle"
 
-const portfolioCard = props => (
-	<PortfolioCardStyle>
-		<div className={props.className}>
-			<div className="card">
-				<img className="card-img-top" src={props.imgSrc} alt="Card image cap" />
-				<div className="card-body">
-					<h5 className="card-title">{props.title}</h5>
-					<p className="card-text">{props.desc}</p>
-					<p>{props.date}</p>
-				</div>
-				<div className="card-hover">
-					<h5>{props.title}</h5>
-					<p>
-						Made with <i className="fas fa-heart" /> and{" "}
-						<i className="fas fa-coffee" />
-					</p>
-					<mark className="mark">Stack &amp; Tools :)</mark>
-					<div className="stack-image">
-						<ul>
-							<li>
-								<i className="fab fa-html5"></i>
-								<span>HTML</span>
-							</li>
-							<li>
-								<i className="fab fa-css3"></i>
-								<span>CSS</span>
-							</li>
-							<li>
-								<i className="fab fa-js"></i>
-								<span>JavaScript</span>
-							</li>
-							<li>
-								<i className="fab fa-bootstrap"></i>
-								<span>Bootstrap</span>
-							</li>
-							<li>
-								<i className="fab fa-font-awesome"></i>
-								<span>FontAwesome</span>
-							</li>
-						</ul>
+const PortfolioCard = props => {
+	// grab project images to pass them as prop
+	const data = useStaticQuery(graphql`
+		query myImgQuery {
+			car: file(relativePath: { eq: "car.jpg" }) {
+				childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			city: file(relativePath: { eq: "chinaCity.jpg" }) {
+				childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			snowfall: file(relativePath: { eq: "snowfall.jpg" }) {
+				childImageSharp {
+					fluid(maxHeight: 325) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			building: file(relativePath: { eq: "building.jpg" }) {
+				childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`)
+	return (
+		<PortfolioCardStyle>
+			<div className={props.className}>
+				<div className="card">
+					<Img
+						className="card-img-top"
+						fluid={props.imgSrc}
+						alt="Card image cap"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">{props.title}</h5>
+						<p className="card-text">{props.desc}</p>
+						<p>{props.date}</p>
 					</div>
-					<hr />
-					<a href={props.githubLink} className=" boxed-btn">
-						View Code
-						<i className="fab fa-github"></i>
-					</a>
-					<a href={props.liveDemo} target="_blank" className="boxed-btn">
-						Live Demo
-						<i className="fas fa-link"></i>
-					</a>
+					<div className="card-hover">
+						<h5>{props.title}</h5>
+						<p>
+							Made with <i className="fas fa-heart" /> and{" "}
+							<i className="fas fa-coffee" />
+						</p>
+						<mark className="mark">Stack &amp; Tools :)</mark>
+						<div className="stack-image">
+							<ul>
+								<li>
+									<i className="fab fa-html5"></i>
+									<span>HTML</span>
+								</li>
+								<li>
+									<i className="fab fa-css3"></i>
+									<span>CSS</span>
+								</li>
+								<li>
+									<i className="fab fa-js"></i>
+									<span>JavaScript</span>
+								</li>
+								<li>
+									<i className="fab fa-bootstrap"></i>
+									<span>Bootstrap</span>
+								</li>
+								<li>
+									<i className="fab fa-font-awesome"></i>
+									<span>FontAwesome</span>
+								</li>
+							</ul>
+						</div>
+						<hr />
+						<a href={props.githubLink} className=" boxed-btn">
+							View Code
+							<i className="fab fa-github"></i>
+						</a>
+						<a href={props.liveDemo} target="_blank" className="boxed-btn">
+							Live Demo
+							<i className="fas fa-link"></i>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
-	</PortfolioCardStyle>
-)
+		</PortfolioCardStyle>
+	)
+}
 
-export default portfolioCard
+export default PortfolioCard
--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from "react"
 import Isotope from "isotope-layout"
+import { graphql, useStaticQuery } from "gatsby"
 import PortfolioStyle from "../styles/PortfolioStyle"
 import ProjectItem from "./ProjectItem"
 import PortfolioCard from "./PortfolioCard"
@@ -29,6 +30,40 @@ const PortfolioItem = () => {
     }
   }, [isotope, filterKey])
 
+  // grab project images to pass them as prop
+  const data = useStaticQuery(graphql`
+    query grabImages {
+      car: file(relativePath: { eq: "car.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 250) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      city: file(relativePath: { eq: "chinaCity.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 250) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      snowfall: file(relativePath: { eq: "snowfall.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 250) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      building: file(relativePath: { eq: "building.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 400, maxHeight: 250) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Fragment>
       <PortfolioStyle>
@@ -47,7 +82,7 @@ const PortfolioItem = () => {
             </div>
             <div className="row filter-container">
               <PortfolioCard
-                imgSrc={PortfolioSectionImg}
+                imgSrc={data.city.childImageSharp.fluid}
                 title="Cool Website"
                 desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati minus qui, non ut animi."
                 date="10 January 2020"
@@ -56,7 +91,7 @@ const PortfolioItem = () => {
                 className="col-md-4 filter-item react"
               />
               <PortfolioCard
-                imgSrc={PortfolioSectionImg}
+                imgSrc={data.car.childImageSharp.fluid}
                 title="Website"
                 desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati minus qui, non ut animi."
                 date="10 January 2020"
@@ -65,7 +100,7 @@ const PortfolioItem = () => {
                 className="col-md-4 filter-item react"
               />
               <PortfolioCard
-                imgSrc={PortfolioSectionImg}
+                imgSrc={data.snowfall.childImageSharp.fluid}
                 title="Awesome ?"
                 desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat obcaecati minus qui, non ut animi."
                 date="10 January 2020"
