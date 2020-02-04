852a819d8d5a2167931e824f5ecabb6e198f88da
Shaonkabir <shaonkabir98@gmail.com>
2020-02-04 22:50:32 +0600

    Style(SectionTitle): update section title style

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -7,27 +7,29 @@ const SectionStyle = styled.div`
 	}
 	.section-title h2 {
 		position: relative;
-		z-index: 1;
-		padding-left: 22px;
-		font-size: 35px;
+		padding: 8px 15px;
+		font-size: 26px;
 		font-weight: 700 !important;
+		display: inline-block;
+		background: ${props => props.theme.color.light};
+		color: ${props => props.theme.color.green};
 	}
 	.section-title h2::before {
 		position: absolute;
 		content: "";
-		left: 0px;
-		top: 6px;
-		width: 5px;
-		height: 30px;
-		background: ${props => props.theme.gradient};
-		transform: rotate(24deg);
+		left: 90%;
+		top: 50%;
+		width: 100%;
+		height: 2px;
+		background: #6b6c6e;
+		z-index: -1;
 	}
 `
 
 const SectionTitle = props => {
 	return (
 		<div className="row">
-			<div className="col text-left">
+			<div className="col">
 				<SectionStyle>
 					<div className="section-title">
 						<h2>{props.title}</h2>
